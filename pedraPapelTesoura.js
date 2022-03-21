974d0fd52ee5ebc3039003cb9e81f61b807ba789
TeodoroWillian <totawillian@gmail.com>
2022-03-21 18:21:17 -0400

    Commit com alterações apresentando escolha do jogador e computador

--- pedraPapelTesoura.js
+++ pedraPapelTesoura.js
@@ -1,10 +1,12 @@
-/* Grifinoria ganha da Sonserina, mas perde para o Corvinal;
+/* Grifinoria ganha da Sonserina, mas perde para a Corvinal;
 Sonserina ganha da Cornival, mas perde para a Grifinoria;
 Corvinal ganha da Grifinoria, mas perde para a Sonserina. */
 
 const prompt = require('prompt-sync')();
 
 console.clear();
+
+console.log("Olá! Escolha uma casa para iniciar o jogo, Grifinoria ganha da Sonserina, mas perde para a Corvinal, Sonserina ganha da Cornival, mas perde para a Grifinoria,Corvinal ganha da Grifinoria, mas perde para a Sonserina. ")
 
 let elementos = ["Grifinoria","Sonserina","Corvinal"]
 let escolhaDoComputador;
@@ -18,7 +20,11 @@ while(desejaJogarNovamente == "sim"){
     for(let i = 0; i< rodadas; i++){
         escolhaDoJogador = prompt("Escolha uma casa para disputa ");
         escolhaDoComputador = Math.floor(Math.random() * 3);
-        console.log(escolhaDoComputador);
+        console.log();
+        console.log(`O computador escolheu ${elementos[escolhaDoComputador]}`);
+        console.log();
+        console.log(`O jogador escolheu: ${escolhaDoJogador}`);
+        console.log();
         if(escolhaDoJogador == "Grifinoria" && elementos[escolhaDoComputador] == "Sonserina"){
             vitoriasJogador++
         } else if(escolhaDoJogador == "Grifinoria" && elementos[escolhaDoComputador] == "Corvinal"){
@@ -44,6 +50,6 @@ while(desejaJogarNovamente == "sim"){
     }
     
     console.log();
-        desejaJogarNovamente = prompt("Deseja jogar novamente?")
+        desejaJogarNovamente = prompt("Deseja jogar novamente?").toLowerCase();
 }
 
